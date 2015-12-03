5a8010e6b4c09fe7ab328513b62c3f2dae41caea
Jesse Connor <jesse@simplefusion.com>
2015-12-03 17:26:38 -0600

    Semi-working version of the group scheduled scripts script

--- GetGroupLevelScheduledScripts.sql
+++ GetGroupLevelScheduledScripts.sql
@@ -9,9 +9,43 @@ Tested Versions   :
 
 Table Aliases     :
   Groups                - mastergroups
-  InternalMonitors      - groupdagents
-  MonitorDetails        - agents
-  GroupCategories       - infocategory
-  AlertTemplates        - alerttemplate
+  ScheduledScripts      - groupscripts
+  Scripts               - lt_scripts
+  Searches              - sensorchecks
 
 */
+
+
+#todo: determine the ScriptFlags use
+#todo: verify each property, get names where it's needed
+#todo: find out why groupscripts contains scripts not related to groups
+
+SELECT
+    CONCAT_WS(' - ', Groups.GroupID, Groups.Name)          AS `GroupName`
+  , Groups.FullName                                        AS `GroupPath`
+  , CONCAT_WS(' - ', Scripts.ScriptId, Scripts.ScriptName) AS `ScriptName`
+  , Searches.Name                                          AS `SearchName`
+  , ScheduledScripts.Priority
+  , ScheduledScripts.Last_Date
+  , ScheduledScripts.SkipOffline
+  , ScheduledScripts.OfflineOnly
+  , ScheduledScripts.WakeOffline
+  , ScheduledScripts.WakeScript
+  , ScheduledScripts.DisableTimeZone
+  , ScheduledScripts.RunScriptOnProbe
+  , ScheduledScripts.RunTime                               AS `NextRun`
+  , ScheduledScripts.ScriptType
+  , ScheduledScripts.Repeat
+  , ScheduledScripts.DistributionWindowType
+  , ScheduledScripts.DistributionWindowAmount
+  , ScheduledScripts.ScheduleType
+  , ScheduledScripts.Interval
+  , ScheduledScripts.RepeatType
+FROM groupscripts AS `ScheduledScripts`
+  LEFT JOIN lt_scripts AS `Scripts` ON ScheduledScripts.ScriptID = Scripts.ScriptId
+  LEFT JOIN mastergroups AS `Groups` ON ScheduledScripts.GroupID = Groups.GroupID
+  LEFT JOIN sensorchecks AS `Searches` ON ScheduledScripts.SearchID = Searches.SensID
+WHERE (Scripts.ScriptFlags & 128) <> 128 AND (Scripts.ScriptFlags & 64) <> 64
+ORDER BY Groups.FullName ASC;
+
+
